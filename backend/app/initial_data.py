2597c115e84d4de9502c8be108c3f487388ff29e
rickyc19 <rickyc19live@gmail.com>
2023-08-18 14:22:30 -0500

    - removed alembic for now

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -3,7 +3,7 @@
 from app.db.session import get_db
 from app.db.crud import create_user
 from app.db.schemas import UserCreate
-from app.db.session import SessionLocal
+from backend.db.session import SessionLocal
 
 
 def init() -> None:
