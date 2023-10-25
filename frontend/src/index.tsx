1c5ce6463b0667b5d44b8ce5f68c45bfcddaa361
rickyc19 <rickyc19live@gmail.com>
2023-10-25 14:16:01 -0500

    Created scrollable bar v1

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Navbar from './components/Navbar/Navbar'
 import EventCard from './components/EventCard/EventCard'
+import FilterBar from './components/FilterBar/FilterBar'
+import ScrollBar from './components/FilterBar/ScrollBar/ScrollBar'
 
 ReactDOM.render(
   <div>
     <Navbar />
+    <FilterBar />
     <div className='home-page-body'>
       <EventCard />
     </div>
